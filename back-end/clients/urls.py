5c7b9c544c9a840c841dbbb20d54e0ec7e118d73
paulo <paulodgsouza@gmail.com>
2022-09-20 08:33:26 -0300

    :sparkles: add requests for client

--- back-end/clients/urls.py
+++ back-end/clients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from clients.views import ClientView
+from clients.views import ClientView, ClientDetailView
 
 urlpatterns = [
     path('', ClientView.as_view()),
+    path("<uuid:client_id>/", ClientDetailView.as_view()),
 ]
--- back-end/clients/views.py
+++ back-end/clients/views.py
@@ -1,9 +1,17 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from clients.models import Client
 from clients.serializers import ClientSerializer
 
 class ClientView(ListCreateAPIView):
+
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientDetailView(RetrieveUpdateDestroyAPIView):
+
+    lookup_url_kwarg = "client_id"
 
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
