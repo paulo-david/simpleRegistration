f421bb73cbf506d7c4a9fd4ec7fdae8feee5ad5f
paulo <paulodgsouza@gmail.com>
2022-09-22 17:02:51 -0300

    :sparkles: basic structure for client CRUD

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import ClientList from "./components/ClientList";
 
 import FormCreateClient from "./components/Forms/formCreateClient";
@@ -9,11 +10,17 @@ import FormGetClient from "./components/Forms/formGetClient";
 const App = () => {
   return (
     <div className="App">
-      
-      <FormCreateClient></FormCreateClient>
-      <FormUpdateClient></FormUpdateClient>
-      <FormDeleteClient></FormDeleteClient>
-      <FormGetClient></FormGetClient>
+      <div className="requests">
+        <section className="column1">
+          <FormCreateClient></FormCreateClient>
+          <FormUpdateClient></FormUpdateClient>
+          <FormDeleteClient></FormDeleteClient>
+        </section>
+        <section className="column2">
+          <FormGetClient></FormGetClient>
+        </section>
+      </div>
+
       <ClientList></ClientList>
     </div>
   );
--- front-end/src/components/ClientData/styles.ts
+++ front-end/src/components/ClientData/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
-const List = styled.div``;
+const List = styled.div`
+  > * {
+    border-radius: 0 0 10px 10px;
+  }
+`;
 
 export default List;
