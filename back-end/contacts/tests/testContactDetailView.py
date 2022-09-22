1dc114d423a9337d729ef109e79e45adb4d3606e
paulo <paulodgsouza@gmail.com>
2022-09-22 18:33:10 -0300

    :heavy_check_mark: add tests to contact

--- back-end/contacts/tests/testContactDetailView.py
+++ back-end/contacts/tests/testContactDetailView.py
@@ -1,6 +1,6 @@
 from model_bakery import baker
 
-from clients.serializers import ClientSerializer
+from contacts.serializers import ContactSerializer
 
 from rest_framework.views import status
 from rest_framework.test import APITestCase
@@ -21,23 +21,23 @@ class TestClientDetaiilView(APITestCase):
             "email": "INVALID",
         }
 
-        cls.path_NOT_FOUND = "/api/clients/39b01442-c799-48ec-b58f-6e91325d9861/"
+        cls.path_NOT_FOUND = "/api/contacts/39b01442-c799-48ec-b58f-6e91325d9861/"
 
     @classmethod
     def setUp(cls) -> None:
 
-        cls.cli = baker.make("clients.Client")
-        cls.path = f"/api/clients/{cls.cli.id}/"
+        cls.contact = baker.make("contacts.Contact")
+        cls.path = f"/api/contacts/{cls.contact.id}/"
 
-    def test_retrieve_client(self):
+    def test_retrieve_contact(self):
 
         response = self.client.get(self.path)
-        expected_response = ClientSerializer(self.cli).data
+        expected_response = ContactSerializer(self.contact).data
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_response)
 
-    def test_retrieve_client_NOT_FOUND(self):
+    def test_retrieve_contact_NOT_FOUND(self):
 
         response = self.client.get(self.path_NOT_FOUND)
         expected_response = {
@@ -47,19 +47,19 @@ class TestClientDetaiilView(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response.data, expected_response)
 
-    def test_update_client(self):
+    def test_update_contact(self):
 
         response = self.client.patch(self.path, self.patch_body)
 
         expected_response = {
-            **ClientSerializer(self.cli).data,
+            **ContactSerializer(self.contact).data,
             **self.patch_body
         }
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_response)
 
-    def test_update_client_with_INVALID_email(self):
+    def test_update_contact_with_INVALID_email(self):
 
         response = self.client.patch(self.path, self.patch_body_INVALID_email)
 
@@ -70,7 +70,7 @@ class TestClientDetaiilView(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, expected_response)
 
-    def test_update_client_NOT_FOUND(self):
+    def test_update_contact_NOT_FOUND(self):
 
         response = self.client.patch(self.path_NOT_FOUND, self.patch_body)
         expected_response = {
@@ -80,13 +80,12 @@ class TestClientDetaiilView(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response.data, expected_response)
 
-    def test_delete_client(self):
+    def test_delete_contact(self):
 
         response = self.client.delete(self.path)
-
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_client_NOT_FOUND(self):
+    def test_delete_contact_NOT_FOUND(self):
 
         response = self.client.delete(self.path_NOT_FOUND)
 
--- back-end/contacts/tests/testContactView.py
+++ back-end/contacts/tests/testContactView.py
@@ -79,7 +79,7 @@ class TestContactView(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, expected_response)
 
-    def test_list_clients(self):
+    def test_list_contacts(self):
 
         response = self.client.get(self.path)
 
